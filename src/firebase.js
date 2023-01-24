137dba53c6e6f7e384ad26a059bcd7e77695b142
Dev By Nav <111112848+Dev-By-Nav@users.noreply.github.com>
2023-01-24 08:04:59 +0000

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBX5zK-OoZpi0vBwu_Xtr0kODW46a420yQ",
+    authDomain: "netflix-clone-d167f.firebaseapp.com",
+    projectId: "netflix-clone-d167f",
+    storageBucket: "netflix-clone-d167f.appspot.com",
+    messagingSenderId: "997181066832",
+    appId: "1:997181066832:web:021b715297ecce148d8b55",
+    measurementId: "G-XE0GXNCP04"
+  };
